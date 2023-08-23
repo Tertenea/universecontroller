4ffda59f57ac90d4c8e6f7cbf38d91675b36cc3f
Tert <56269917+Tertenea@users.noreply.github.com>
2023-08-23 03:55:20 +0300

    Add files via upload

---
+++ universe controller/characters/enemy/Enemy.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+export (int) var speed = 150
+
+var hp = 1
+
+func _physics_process(delta):
+	global_position.y += speed * delta
+
+func take_damage(damage):
+	hp -= damage
+	if hp <= 0:
+		queue_free()
+
+func _on_Enemy_area_entered(area):
+	if area is Player:
+		area.take_damage(1)
---
+++ universe controller/characters/enemy/EnemySpawner.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var spawn_positions = null
+
+var Enemy = preload("res://characters/enemy/Enemy.tscn")
+
+func _ready():
+	randomize()
+	spawn_positions = $SpawnPositions.get_children()
+
+func spawn_enemy():
+	var index = randi() % spawn_positions.size()
+	var enemy = Enemy.instance()
+	enemy.global_position = spawn_positions[index].global_position
+	add_child(enemy)
+
+func _on_SpawnTimer_timeout():
+	spawn_enemy()
